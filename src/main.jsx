8c2a51f549564fd8d52d1ce2ae2cd643ba483dbb
mufizul27 <cse.mufizul@gmail.com>
2023-04-08 10:16:28 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import OrderReviews from './Components/OrderReviews/OrderReviews';
+
+
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+   children:[
+
+    {
+     path:'/',
+     element:<Home></Home>,
+     loader:() => fetch('tshirts.json')
+
+    },
+    {
+      path:'review',
+      element:<OrderReviews></OrderReviews>
+    }
+   ]
+
+
+  },
+
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
